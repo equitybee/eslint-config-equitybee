6f955201c53e82ff0c66fdb6f3dce49f279ac444
Jonathan Levi <jonathan1996levi@gmail.com>
2021-10-08 13:21:49 +0100

    [CustomRule][Test] Add no-index rule, add mocha test runner

---
+++ tests/lib/rules/no-index.js
@@ -0,0 +1,40 @@
+const indexRule = require("../../../lib/rules/no-index");
+const RuleTester = require("eslint").RuleTester;
+
+const testCode = "var bee = 'honey';";
+const ruleTester = new RuleTester();
+
+ruleTester.run("no-index", indexRule, {
+  valid: [
+    {
+      code: testCode,
+      filename: "equitybee.js",
+    },
+    {
+      code: testCode,
+      filename: "/some/dir/equitybee.js",
+    },
+  ],
+  invalid: [
+    {
+      code: testCode,
+      filename: "index.js",
+      errors: [{ message: "Files named 'index' are not allowed.", column: 1, line: 1 }],
+    },
+    {
+      code: testCode,
+      filename: "/some/dir/index.js",
+      errors: [{ message: "Files named 'index' are not allowed.", column: 1, line: 1 }],
+    },
+    {
+      code: testCode,
+      filename: "/some/dir/index-equitybee.js",
+      errors: [{ message: "Files named 'index' are not allowed.", column: 1, line: 1 }],
+    },
+    {
+      code: testCode,
+      filename: "/some/dir/equitybeeIndex.js",
+      errors: [{ message: "Files named 'index' are not allowed.", column: 1, line: 1 }],
+    },
+  ],
+});
